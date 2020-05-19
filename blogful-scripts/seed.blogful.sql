8358c3e4397164fc4969731477827826fbfa2e48
Tyler Alder <tyler.b.alder@gmail.com>
2020-05-19 05:09:52 -0700

    added shopping list services

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,102 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+  (
+    'Article One',
+    'Some lipsum orem nonsense a',
+    now() - '21 days' :: INTERVAL
+  ),
+  (
+    'Article Two',
+    'Some lipsum orem nonsense b',
+    now() - '22 days' :: INTERVAL
+  ),
+  (
+    'Article Three',
+    'Some lipsum orem nonsense c',
+    now() - '23 days' :: INTERVAL
+  ),
+  (
+    'Article Four',
+    'Some lipsum orem nonsense d',
+    now() - '21 days' :: INTERVAL
+  ),
+  (
+    'Article Five',
+    'Some lipsum orem nonsense e',
+    now() - '25 days' :: INTERVAL
+  ),
+  (
+    'Article Six',
+    'Some lipsum orem nonsense',
+    now() - '26 days' :: INTERVAL
+  ),
+  (
+    'Article Seven',
+    'Some lipsum orem nonsense f',
+    now() - '27 days' :: INTERVAL
+  ),
+  (
+    'Article Eight',
+    'Some lipsum orem nonsense g',
+    now() - '28 days' :: INTERVAL
+  ),
+  (
+    'Article Nine',
+    'Some lipsum orem nonsense h',
+    now() - '29 days' :: INTERVAL
+  ),
+  (
+    'Article Ten',
+    'Some lipsum orem nonsense i',
+    now() - '30 days' :: INTERVAL
+  ),
+  (
+    'Article Eleven',
+    'Some lipsum orem nonsense j',
+    now() - '31 days' :: INTERVAL
+  ),
+  (
+    'Article Twelve',
+    'Some lipsum orem nonsense k',
+    now() - '32 days' :: INTERVAL
+  ),
+  (
+    'Article Thirteen',
+    'Some lipsum orem nonsense l',
+    now() - '33 days' :: INTERVAL
+  ),
+  (
+    'Article Fourteen',
+    'Some lipsum orem nonsense m',
+    now() - '34 days' :: INTERVAL
+  ),
+  (
+    'Article Fifteen',
+    'Some lipsum orem nonsense n',
+    now() - '35 days' :: INTERVAL
+  ),
+  (
+    'Article Sixteen',
+    'Some lipsum orem nonsense o',
+    now() - '36 days' :: INTERVAL
+  ),
+  (
+    'Article Seventeen',
+    'Some lipsum orem nonsense p',
+    now() - '37 days' :: INTERVAL
+  ),
+  (
+    'Article Eighteen ',
+    'Some lipsum orem nonsense q',
+    now() - '38 days' :: INTERVAL
+  ),
+  (
+    'Article Nineteen',
+    'Some lipsum orem nonsense r',
+    now() - '39 days' :: INTERVAL
+  ),
+  (
+    'Article Twenty',
+    'Some lipsum orem nonsense s',
+    now() - '40 days' :: INTERVAL
+  );
